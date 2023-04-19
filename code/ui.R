6bd6455a103fc98b41b7ea1199b149148abdb432
Shlok Patel <shlokpat6@gmail.com>
2023-04-19 14:31:22 -0500

    Added front end for automatic fitting

--- code/ui.R
+++ code/ui.R
@@ -98,7 +98,24 @@ ui <- navbarPage(title = "MeltShiny",
                                                               )
                                                             )
                                                           ),
-                                                 tabPanel(title = "Automatic")
+                                                 tabPanel(title = "Automatic",
+                                                          fluidPage(
+                                                            sidebarLayout(
+                                                              sidebarPanel(
+                                                                textInput(label = "Enter the number of iterations.",
+                                                                          placeholder = "E.g: 1000",
+                                                                          value = 1000,
+                                                                          inputId = "autoFitIterationsID"
+                                                                ),
+                                                                h5("Click to automatically fit all graphs based on chosen iterations."),
+                                                                actionButton(inputId = "automaticFitID",
+                                                                             label = "Fit Data"
+                                                                             )
+                                                                ),
+                                                              mainPanel()
+                                                              )
+                                                            )
+                                                          )
                                                  )
                                      )
                             ),
