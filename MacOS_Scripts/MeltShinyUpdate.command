f6eb8bfed577761ed6b840ae93ef90c69c64adfe
Zohaib Ahmed <zohaib0718@gmail.com>
2023-09-29 14:05:01 -0500

    Specified 'code' subdirectory + parent dir check

--- MacOS_Scripts/MeltShinyUpdate.command
+++ MacOS_Scripts/MeltShinyUpdate.command
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-ZIP_URL="https://github.com/oss-slu/MeltWin2.0/archive/refs/heads/main.zip"
-
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 PROGRAM_DIR="$(dirname "$SCRIPT_DIR")"
+
+# Get the name of the program directory
+PROGRAM_NAME="$(basename "$PROGRAM_DIR")"
+
+# Check if the program directory is named 'MeltWin2.0-main'
+if [ "$PROGRAM_NAME" != "MeltWin2.0-main" ]; then
+    echo "Error: The parent directory is not 'MeltWin2.0-main'! Exiting..."
+    exit 1
+fi
+
+ZIP_URL="https://github.com/oss-slu/MeltWin2.0/archive/refs/heads/main.zip"
+CODE_DIR="$PROGRAM_DIR/code" # specify the Code subdirectory
 
 # Create a temporary directory
 mkdir -p "/tmp/UpdateTemp"
@@ -12,14 +22,13 @@ curl -L -o "/tmp/UpdateTemp/latest.zip" "$ZIP_URL"
 # Unzip the archive to the temporary location
 unzip -o "/tmp/UpdateTemp/latest.zip" -d "/tmp/UpdateTemp"
 
-# Delete all existing files and directories inside MeltWin2.0, excluding MacOS_Scripts and Windows_Scripts
-find "$PROGRAM_DIR" -mindepth 1 -maxdepth 1 ! -name 'MacOS_Scripts' ! -name 'Windows_Scripts' -exec rm -rf {} \;
-# Copy the contents of the unzipped folder to the program directory
-cp -R "/tmp/UpdateTemp/MeltWin2.0-main/"* "$PROGRAM_DIR"
+# Delete only the 'code' subdirectory inside PROGRAM_DIR
+rm -rf "$CODE_DIR"
+# Copy the 'code' subdirectory from the unzipped folder to PROGRAM_DIR
+cp -R "/tmp/UpdateTemp/MeltWin2.0-main/code" "$PROGRAM_DIR"
 
 # Clean up the temporary directory
 rm -r "/tmp/UpdateTemp"
 
-echo "Update complete! Your program is now up to date."
+echo "Update complete! Your program's 'code' subdirectory is now up to date."
 exit 0
-
--- Windows_Scripts/MeltShinyUpdate.bat
+++ Windows_Scripts/MeltShinyUpdate.bat
@@ -12,6 +12,19 @@ cd %SCRIPT_DIR%\..
 set PROGRAM_DIR=%cd%
 cd %SCRIPT_DIR%
 
+:: Get the name of the program directory
+for %%a in (%PROGRAM_DIR%) do set PROGRAM_NAME=%%~nxa
+
+:: Check if the program directory is named 'MeltWin2.0-main'
+if /I not "%PROGRAM_NAME%"=="MeltWin2.0-main" (
+    echo Error: The parent directory is not 'MeltWin2.0-main'! Exiting...
+    pause
+    exit /b 1
+)
+
+:: Define the 'code' subdirectory inside PROGRAM_DIR
+set CODE_DIR=%PROGRAM_DIR%\code
+
 :: Create a temporary directory
 set TEMP_DIR=%TEMP%\UpdateTemp
 mkdir "%TEMP_DIR%"
@@ -20,17 +33,13 @@ powershell -Command "& { Invoke-WebRequest -Uri '%ZIP_URL%' -OutFile '%TEMP_DIR%
 :: Unzip the archive to a temporary location
 powershell -Command "& { Expand-Archive -Path '%TEMP_DIR%\latest.zip' -DestinationPath '%TEMP_DIR%' -Force }"
 
-:: Delete all existing files and directories inside MeltWin2.0, excluding Windows_Scripts and MacOS_Scripts
-for /D %%A in ("%PROGRAM_DIR%\*") do (
-    if /I not "%%~nxA"=="Windows_Scripts" if /I not "%%~nxA"=="MacOS_Scripts" (
-        rd /s /q "%%~fA"
-    )
-)
-:: Copy the contents of the unzipped folder to the program directory
-xcopy /E /I /Y "%TEMP_DIR%\MeltWin2.0-main\*" "%PROGRAM_DIR%\"
+:: Delete the existing 'code' subdirectory inside PROGRAM_DIR
+rd /s /q "%CODE_DIR%"
+:: Copy the 'code' subdirectory from the unzipped folder to PROGRAM_DIR
+xcopy /E /I /Y "%TEMP_DIR%\MeltWin2.0-main\code\*" "%CODE_DIR%\"
 
 :: Clean up the temporary directory
 rd /s /q "%TEMP_DIR%"
 
-echo Update complete! Your program is now up to date.
+echo Update complete! Your program's 'code' subdirectory is now up to date.
 pause
