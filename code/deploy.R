71e19f7d76e105d3b9a710be22b60dca51d629e5
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-03-14 15:33:25 -0500

    solving deployment error

--- code/deploy.R
+++ code/deploy.R
@@ -25,5 +25,5 @@ setAccountInfo(name = error_on_missing_name("SHINY_ACC_NAME"),
 # Deploy the application.
 deployApp(
     appName = "MeltShiny",
-    appFiles = c("ui.R", "server.r", "global.r", "app.R", "install.R", "deploy.R","www"), 
+    appFiles = c("ui.R", "server.r", "global.r", "app.R", "install.R", "deploy.R","www", "modules_1.r.", "modules_2", "uiParts"), 
     forceUpdate = TRUE )
